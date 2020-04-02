6303128af92ca892e9b2344025ff5b5c836c3087
auusui <60081732+auusui@users.noreply.github.com>
2020-04-01 18:03:10 -1000

    contacts 2 finished in 17 minutes

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,18 +1,17 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
-
+import { Contacts } from '../../api/contact/Contacts.js';
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
-function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.insert(data);
+function addContact(data) {
+  console.log(`  Adding: ${data.lastName} (${data.owner})`);
+  Contacts.insert(data);
 }
 
 /** Initialize the collection if empty. */
-if (Stuffs.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
+if (Contacts.find().count() === 0) {
+  if (Meteor.settings.defaultContact) {
+    console.log('Creating contact data.');
+    Meteor.settings.defaultContacts.map(data => addContact(data));
   }
 }
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,20 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
-import { Stuffs } from '../../api/stuff/Stuff';
+import { Contacts } from '../../api/contact/Contacts';
 
 /** This subscription publishes only the documents associated with the logged in user */
-Meteor.publish('Stuff', function publish() {
+Meteor.publish('Contacts', function publish() {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
-    return Stuffs.find({ owner: username });
+    return Contacts.find({ owner: username });
   }
   return this.ready();
 });
 
 /** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
-Meteor.publish('StuffAdmin', function publish() {
+Meteor.publish('ContactAdmin', function publish() {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.find();
+    return Contacts.find();
   }
   return this.ready();
 });

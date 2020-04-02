b8fa769cb099c339d666e331778e3203a162a16c
auusui <60081732+auusui@users.noreply.github.com>
2020-04-01 16:03:21 -1000

    contacts collection finished in 28 minutes

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
-
+import { Contact } from '../../api/contact/Contact.js';
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
@@ -14,5 +13,19 @@ if (Stuffs.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+/** Initialize the database with a default data document. */
+function addContact(data) {
+  console.log(`  Adding: ${data.lastName} (${data.owner})`);
+  Contact.insert(data);
+}
+
+
+if (Contact.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating contact data.');
+    Meteor.settings.defaultContacts.map(data => addContact(data));
   }
 }
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
-import { Stuffs } from '../../api/stuff/Stuff';
+import { Stuffs } from '../../api/contact/Stuff';
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Stuff', function publish() {
@@ -15,6 +15,24 @@ Meteor.publish('Stuff', function publish() {
 Meteor.publish('StuffAdmin', function publish() {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Stuffs.find();
+  }
+  return this.ready();
+});
+
+/** This subscription publishes only the documents associated with the logged in user */
+Meteor.publish('Contacts', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userID).username;
+    return Contacts.find({ owner: username });
+  }
+  return this.ready();
+});
+
+
+/** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
+Meteor.publish('ContactsAdmin', function publish() {
+  if (this.username && Roles.userIsInRole(this.userId, 'admin')) {
+    return Contacts.find();
   }
   return this.ready();
 });
